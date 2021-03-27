643c89b650cab38417602a0cdec16becb7dc22d8
sagewu <sq13592829287@gmail.com>
2021-03-27 14:32:02 +0800

    callback example add comment

--- src/3_callback/main.cc
+++ src/3_callback/main.cc
@@ -4,7 +4,13 @@ using namespace v8;
 
 void Run(const FunctionCallbackInfo<Value>& info) {
   Isolate* isolate = info.GetIsolate();
+  Local<Context> context = isolate->GetCurrentContext();
+  // 进入context
+  Context::Scope enter_context(context);
+  // 创建scope
+  HandleScope scope();
 
+  // 检查参数
   if (info.Length() < 1) {
     isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "需传入回调")));
   }
@@ -13,14 +19,16 @@ void Run(const FunctionCallbackInfo<Value>& info) {
   }
 
   Local<Function> cb = Local<Function>::Cast(info[0]);
-
+  // 调用回调函数
   const int argc = 1;
   Local<Value> argv[argc] = { String::NewFromUtf8(isolate, "data") };
-  cb->Call(isolate->GetCurrentContext(), Null(isolate), argc, argv);
+  cb->Call(context, Null(isolate), argc, argv);
 }
 
+// 初始化模块
 void Init(Local<Object> exports) {
   NODE_SET_METHOD(exports, "run", Run);
 }
 
+// 声明模块
 NODE_MODULE(NODE_GYP_MODULE_NAME, Init);
